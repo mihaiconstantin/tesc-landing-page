31de9d1d9d4d3c56047397c9b4b94db5cfbdef1e
mihaiconstantin <c.tinmihai@ymail.com>
2018-02-01 11:51:05 +0100

    Compiling different (smaller) styles & scripts for each page

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// Lading page.
+mix.js('resources/assets/js/entries/index.js', 			'public/js')
+   .sass('resources/assets/sass/entries/index.scss', 	'public/css')
+   .options({ extractVueStyles: true });
+   
+
+// Project pages.
+mix.js('resources/assets/js/entries/project.js', 		'public/js')
+   .sass('resources/assets/sass/entries/project.scss',	'public/css');
