37df0746bb7a98cbe11934b00edbc118d18f5666
mihaiconstantin <c.tinmihai@ymail.com>
2018-02-01 11:48:27 +0100

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,38 @@
 
 Route::get('/', function () {
 	return view('index');
-});
+})->name('index');
 
 
+/**
+ * Routes that will be called by Axios to populate the Vuex Store.
+ */
+Route::get('api/all', 'ApiController@all');
+Route::get('api/carousel', 'ApiController@carousel');
+Route::get('api/about', 'ApiController@about');
+Route::get('api/people', 'ApiController@people');
+Route::get('api/project', 'ApiController@project');
+Route::get('api/contact', 'ApiController@contact');
+Route::get('api/subscription', 'ApiController@subscription');
+
+
+/**
+ * Routes that handle POST requests (e.g., storing a contact message to the database).
+ */
+Route::post('api/message', 'ApiController@storeMessage')->name('message.store');
+Route::post('api/subscription', 'ApiController@storeSubscription')->name('subscription.store');
+
+
+/*
+ * Routes for the project pages.
+ */
+Route::get('project/{slug}', 'ProjectController@show');
+
+
+
+/*
+ * Voyager admin routes.
+ */
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
