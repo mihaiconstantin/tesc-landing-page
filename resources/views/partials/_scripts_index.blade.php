41c671469da927a48ae2f746e955312650e09808
mihaiconstantin <c.tinmihai@ymail.com>
2018-02-02 18:35:21 +0100

    Added partial view for Google Analytics script

--- resources/views/partials/_scripts_index.blade.php
+++ resources/views/partials/_scripts_index.blade.php
@@ -2,3 +2,5 @@
 <script src="{{ URL::asset('js/index.js') }}"></script>
 <script src="https://platform.twitter.com/widgets.js" async charset="utf-8"></script>
 <script src='https://www.google.com/recaptcha/api.js' async defer></script>
+
+@include('partials._analytics')
--- resources/views/partials/_scripts_project.blade.php
+++ resources/views/partials/_scripts_project.blade.php
@@ -1,2 +1,4 @@
 <script src="{{ URL::asset('js/project.js') }}"></script>
 <script src="https://platform.twitter.com/widgets.js" async></script>
+
+@include('partials._analytics')
