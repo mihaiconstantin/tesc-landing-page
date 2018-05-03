7c8597cd08a4ce4cd360ba7d6e2a588fbc48d66a
mihaiconstantin <c.tinmihai@ymail.com>
2018-05-03 07:58:19 +0200

    Changed all rendered views to follow the strucure of master template

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,39 +1,52 @@
 @extends('templates.blog')
 
 
+@section('meta_seo')
+	<meta name="description" content="{{ setting('blog.description') }}">
+	<meta name="keywords" content="{{ setting('blog.keywords') }}">
+@endsection
+
+
+@section('styles')
+	@include('partials._styles_blog')
+@endsection
+
+
 @section('title', 'Blog')
 	
 
 @section('content')
-<div id="app-blog" class="br">
+	<div id="app-blog" class="br">
+		<app-word-animation v-if="!loadingCompleted" :animationoff="true"></app-word-animation>
+		<template v-else>
+			<!-- Blog navigation bar. -->
+			<app-blog-navbar logo="{{ setting('site.navbar_logo') }}" set-active-link="Blog"></app-blog-navbar>
 
-	<app-word-animation v-if="!loadingCompleted" :animationoff="true"></app-word-animation>
+			<main id="app-blog-main" class="bg container">
+				<div class="br app-blog-content row">
+					<!-- Pins. -->
+					<i class="pin pin-top-left"></i>
+					<i class="pin pin-top-right"></i>
+					<i class="pin pin-bottom-left"></i>
+					<i class="pin pin-bottom-right"></i>
 
-	<template v-else>
-		<!-- Blog navigation bar. -->
-		<app-blog-navbar logo="{{ setting('site.navbar_logo') }}" set-active-link="Blog"></app-blog-navbar>
+					<!-- Blog header & filter input. -->
+					<app-blog-header></app-blog-header>
 
-		<main id="app-blog-main" class="bg container">
-			<div class="br app-blog-content row">
-				<!-- Pins. -->
-				<i class="pin pin-top-left"></i>
-				<i class="pin pin-top-right"></i>
-				<i class="pin pin-bottom-left"></i>
-				<i class="pin pin-bottom-right"></i>
+					<!-- Blog posts. -->
+					<app-blog-post-list></app-blog-post-list>
 
-				<!-- Blog header & filter input. -->
-				<app-blog-header></app-blog-header>
+					<hr class="col-5 mx-auto">
 
-				<!-- Blog posts. -->
-				<app-blog-post-list></app-blog-post-list>
-				
-				<hr class="col-5 mx-auto">
+					<!-- Blog footer. -->
+					<app-blog-footer twitter-icon="{{ setting('site.twitter_logo') }}"></app-blog-footer>
+				</div>
+			</main>
+		</template>
+	</div>
+@endsection
 
-				<!-- Blog footer. -->
-				<app-blog-footer twitter-icon="{{ setting('site.twitter_logo') }}"></app-blog-footer>
-			</div>
-		</main>
-	</template>
 
-</div>
+@section('scripts')
+	@include('partials._scripts_blog')
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,38 @@
 @extends('templates.index')
 
+
+@section('meta_seo')
+	<meta name="description" content="{{ setting('site.description') }}">
+	<meta name="keywords" content="{{ setting('site.keywords') }}">
+@endsection
+
+
+@section('styles')
+	@include('partials._styles_index')
+@endsection
+
+
+@section('title', setting('site.title'))
+
+
 @section('content')
+	<div id="app" class="bp">
+		<app-word-animation v-if="!loadingCompleted" :animationoff="animationStatus"></app-word-animation>
+		<template v-else>
+			<app-main-navbar></app-main-navbar>
+			<div class="bs container-fluid">
+				<app-carousel></app-carousel>
+				<app-about></app-about>
+				<app-people></app-people>
+				<app-projects></app-projects>
+				<app-contact></app-contact>
+				<app-footer></app-footer>
+			</div>
+		</template>
+	</div>
+@endsection
 
-<div id="app" class="bp">
-	<app-word-animation v-if="!loadingCompleted" :animationoff="animationStatus"></app-word-animation>
 
-	<template v-else>
-		<app-main-navbar></app-main-navbar>
-
-		<div class="bs container-fluid">
-			<app-carousel></app-carousel>
-			<app-about></app-about>
-			<app-people></app-people>
-			<app-projects></app-projects>
-			<app-contact></app-contact>
-			<app-footer></app-footer>
-		</div>
-	</template>
-</div>
+@section('scripts')
+	@include('partials._scripts_index')
 @endsection
