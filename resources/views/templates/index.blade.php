69a22f429116b20063b4c4128033eb3922c139a7
mihaiconstantin <c.tinmihai@ymail.com>
2018-05-03 07:56:33 +0200

    Forced all view templates to extend the master template

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<!-- SEO meta tags -->
-	<meta name="description" content="{{ setting('site.description') }}">
-	<meta name="keywords" content="{{ setting('site.keywords') }}">
+@extends('templates.master')
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon">
-    @include('partials._styles_index')
-
-    <title>{{ setting('site.title') }}</title>
-</head>
-
-<body>
-    @yield('content')
-    @include('partials._scripts_index')
-</body>
-
-<!-- By Mihai Constantin - m.a.constantin@uvt.nl -->
-</html>
+@section('ie_scripts')
+    @include('partials._ie_scripts')
+@endsection
