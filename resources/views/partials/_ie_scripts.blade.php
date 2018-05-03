54af2c85f35e42a44490190e33b717054e01f83d
mihaiconstantin <c.tinmihai@ymail.com>
2018-05-03 08:01:53 +0200

    Added conditional polyfill script for IE of version 11

---
+++ resources/views/partials/_ie_scripts.blade.php
@@ -0,0 +1,4 @@
+<!-- Scripts for supporting Internet Explorer. -->
+@supportedInternetExplorerVersion
+    <script src='https://cdn.polyfill.io/v2/polyfill.min.js'></script>
+@endsupportedInternetExplorerVersion
